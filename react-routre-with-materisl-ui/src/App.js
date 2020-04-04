618395a6e5a28c59740c90a131cfeaa29fc00190
DebarunMitra <debarun96mitra@gmail.com>
2020-04-04 22:39:41 +0530

    route added

--- react-routre-with-materisl-ui/src/App.js
+++ react-routre-with-materisl-ui/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link  } from 'react-router-dom';
+
 
 export default class extends Component{
+  state={
+    writersSet:[]
+  }
+
+  async componentDidMount(){
+    const writers = await (await fetch('http://localhost:3004/writers')).json();
+
+    // console.log(writers);
+    this.setState({
+      writersSet: writers 
+    })
+    
+  }
+
     render(){
       return (
-        <div>
-          
-        </div>
+        <BrowserRouter>
+          <div>
+            <ul>
+              <li>
+                <link to="/">Home</link>
+              </li>
+              <li>
+                <link to="/writers">Writer</link>
+              </li>
+            </ul>
+          </div>
+        </BrowserRouter>
       )
     }
 }
